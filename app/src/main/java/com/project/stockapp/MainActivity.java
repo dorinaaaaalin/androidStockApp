95004c2b48a4a4ff946eb745194f70fc21a47e76
Jeff Liu <boyuanliu@live.com>
2018-07-28 20:36:59 -0400

    Added Alpha Vantage api call

--- app/src/main/java/com/project/stockapp/MainActivity.java
+++ app/src/main/java/com/project/stockapp/MainActivity.java
@@ -2,19 +2,29 @@ package com.project.stockapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.*;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String ALPHA_VINTAGE_URL = "https://www.alphavantage.co/query";
 
     // Member Variables:
     TextView mPriceTextView;
+    Button mTestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,45 +32,52 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPriceTextView = (TextView) findViewById(R.id.priceLabel);
-        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
-
-        // Create an ArrayAdapter using the String array and a spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.currency_array, R.layout.spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        mTestButton = (Button) findViewById(R.id.go_get_it_button);
 
         // TODO: Set an OnItemSelected listener on the spinner
-
+        mTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //hardcode for now when calling function
+                RequestParams params = new RequestParams();
+                params.put("function", "TIME_SERIES_INTRADAY");
+                params.put("symbol", "MSFT");
+                params.put("interval", "1min");
+                params.put("apikey", "WKU9BI51SRFH9VW6");
+                letsDoSomeNetworking(ALPHA_VINTAGE_URL, params);
+            }
+        });
     }
 
     // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
+    private void letsDoSomeNetworking(String url, RequestParams params) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        AlphaVantageApiClient.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+//                Log.d("StockApp", "JSON: " + response.toString());
+                try {
+                    JSONObject metaObject = response.getJSONObject("Meta Data");
+                    String symbol = metaObject.getString("2. Symbol");
+                    String lastRefreshed = metaObject.getString("3. Last Refreshed");
+
+                    Log.d("StockApp", "symbol: " + symbol);
+                    Log.d("StockApp", "high: " + lastRefreshed);
+                    Log.d("StockApp", "Dorina: Gay as fuck");
+
+
+//                updateUI
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
 
 
     }
